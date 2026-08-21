d29ea103e8dac627ba7ee0fcd3566b96092c29ca
Syntax-Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2026-08-20 22:26:10 -0700

    chore: namespace cleanup

--- src/proc_monitor.cpp
+++ src/proc_monitor.cpp
@@ -10,49 +10,51 @@
 
 #include "proc_monitor.h"
 
-uint64_t ProcMonitor::compute_total_time(const CPUTimes& times) {
-    return
-        times.idle_time +
-        times.io_wait +
-        times.irq +
-        times.nice_time +
-        times.soft_irq +
-        times.steal_time +
-        times.system_time +
-        times.user_time;
-}
-
-uint64_t ProcMonitor::compute_idle_time(const CPUTimes& times) {
-    return times.idle_time + times.io_wait;
-}
-
-std::optional<double> ProcMonitor::compute_utilization(
-    const CPUTimes& prev, 
-    const CPUTimes& curr
-) {
-    uint64_t total_prev = ProcMonitor::compute_total_time(prev);
-    uint64_t total_curr = ProcMonitor::compute_total_time(curr);
-
-    if (total_curr < total_prev) {
-        return std::nullopt;
+namespace ProcMonitor {
+    uint64_t compute_total_time(const CPUTimes& times) {
+        return
+            times.idle_time +
+            times.io_wait +
+            times.irq +
+            times.nice_time +
+            times.soft_irq +
+            times.steal_time +
+            times.system_time +
+            times.user_time;
     }
 
-    uint64_t total_delta = total_curr - total_prev;
-
-    if (total_delta == 0) {
-        return 0.0;
+    uint64_t compute_idle_time(const CPUTimes& times) {
+        return times.idle_time + times.io_wait;
     }
 
-    uint64_t idle_time_prev = ProcMonitor::compute_idle_time(prev);
-    uint64_t idle_time_curr = ProcMonitor::compute_idle_time(curr);
+    std::optional<double> compute_utilization(
+        const CPUTimes& prev, 
+        const CPUTimes& curr
+    ) {
+        uint64_t total_prev = ProcMonitor::compute_total_time(prev);
+        uint64_t total_curr = ProcMonitor::compute_total_time(curr);
 
-    if (idle_time_curr < idle_time_prev) {
-        return std::nullopt;
+        if (total_curr < total_prev) {
+            return std::nullopt;
+        }
+
+        uint64_t total_delta = total_curr - total_prev;
+
+        if (total_delta == 0) {
+            return 0.0;
+        }
+
+        uint64_t idle_time_prev = ProcMonitor::compute_idle_time(prev);
+        uint64_t idle_time_curr = ProcMonitor::compute_idle_time(curr);
+
+        if (idle_time_curr < idle_time_prev) {
+            return std::nullopt;
+        }
+        
+        uint64_t idle_delta = idle_time_curr - idle_time_prev;
+        
+        double utilization = 100.0 * (total_delta - idle_delta) / total_delta;
+
+        return utilization;
     }
-    
-    uint64_t idle_delta = idle_time_curr - idle_time_prev;
-    
-    double utilization = 100.0 * (total_delta - idle_delta) / total_delta;
-
-    return utilization;
-}
+};
